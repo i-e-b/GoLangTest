874d3cbd0111e0092bd259b723cc87783d0cc123
Iain Ballard <iain.ballard@bjss.com>
2021-08-27 12:57:53 +0100

    Server cookie test

--- LittleWebServer/main_lws_test.go
+++ LittleWebServer/main_lws_test.go
@@ -15,11 +15,63 @@ func TestScanning(t *testing.T){
 	fmt.Printf("Got back %v\r\n", bits)
 }
 
+func TestPasswordProtection(t *testing.T){
+	SetUpLogging(false, false)
+	server :=  &LittleServer{userDb: map[int]MyInputType{}}
+	server.userDb[0] = MyInputType{
+		ID:   123,
+		Name: "Test user",
+		Age:  22,
+	}
+
+	expectedRejection := `{"error":"must provide token cookie"}`
+	expectedSuccess := `{"id":123,"name":"Test user","age":22}`
+	loginString := `{ "username": "ieb", "password": "correct" }`
+
+
+	// First, should be rejected without a token
+	request, _ := http.NewRequest(http.MethodGet, "http://localhost:6080/user", nil)
+	response := httptest.NewRecorder()
+
+	server.ServeHTTP(response, request)
+	if response.Code != 401 {
+		t.Errorf("Should have been rejected with 401, but got %v", response.Code)
+	}
+	actual := response.Body.String()
+	if actual != expectedRejection{t.Errorf("Expected '%s', but got '%s'", expectedRejection, actual)}
+
+	// Next, I should be able to log in
+	request, _ = http.NewRequest(http.MethodPost, "http://localhost:6080/login", strings.NewReader(loginString))
+	response = httptest.NewRecorder()
+	server.ServeHTTP(response, request)
+	if response.Code != 200 {t.Errorf("Should have been accepted with 200, but got %v", response.Code)}
+	cookieValue := response.Header().Get("Set-Cookie")
+	if !startsWith(cookieValue, "token="){
+		t.Errorf("Should have got 'token=...', but got %v", cookieValue)
+	}
+
+	// Finally, make the original call, but with the cookieValue set
+	request, _ = http.NewRequest(http.MethodGet, "http://localhost:6080/user/0", nil)
+	request.Header.Set("Cookie", cookieValue)
+	response = httptest.NewRecorder()
+
+	server.ServeHTTP(response, request)
+	if response.Code != 200 {
+		t.Errorf("Should have been accepted with 200, but got %v", response.Code)
+	}
+	actual = response.Body.String()
+	if actual != expectedSuccess {
+		t.Errorf("Expected '%s', but got '%s'", expectedSuccess, actual)
+	}
+}
+
+func startsWith(haystack, needle string)bool{return strings.Index(haystack, needle) == 0 }
+
 func TestLittleServer_ServeHTTP(t *testing.T) {
 	t.Run("Do a get", func(t *testing.T) {
 		SetUpLogging(false, false)
 		request, _ := http.NewRequest(http.MethodGet, "http://localhost:6080/api/people/123", nil)
-		response := httptest.NewRecorder() // built-in mocks! :-)
+		response := httptest.NewRecorder() // built-in mocks! :-D
 
 		server := &LittleServer{}
 		server.ServeHTTP(response, request)
