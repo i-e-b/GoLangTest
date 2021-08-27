fee5b1c472a56b7ac73fa036ac3dded3595d3a78
Iain Ballard <iain.ballard@bjss.com>
2021-08-27 10:58:00 +0100

    Targetable logging

--- LittleWebServer/main_lws.go
+++ LittleWebServer/main_lws.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -24,14 +26,12 @@ type LittleServer struct {
 	userDb  map[int]MyInputType
 }
 
-var useLogFile = false
-
 var infoLog *log.Logger
 var warnLog *log.Logger
 var critLog *log.Logger
 
 func main(){
-	logFile := setUpLogging()
+	logFile := setUpLogging(false, false)
 	defer func(file *os.File) { if file == nil {return}; _ = file.Close() }(logFile)
 
 	infoLog.Printf("Bringing up a server on http://localhost%s\r\n", httpPort)
@@ -39,15 +39,22 @@ func main(){
 	server := &LittleServer{
 		userDb: map[int]MyInputType{},
 	}
+	// add a sample user at [0]
+	server.userDb[0] = MyInputType{
+		ID:   -1,
+		Name: "Sample user",
+		Age:  20,
+	}
+
 	http.Handle("/", server)
 
 	err := http.ListenAndServe(httpPort, nil)
 	if err != nil {
-		critLog.Printf("Server failed: %v", err)
+		critLog.Fatalf("Server failed: %v", err)
 	}
 }
 
-func setUpLogging() (usingFile *os.File) {
+func setUpLogging(useLogFile, onlyImportant bool) (usingFile *os.File) {
 	logTarget := os.Stderr
 	if useLogFile {
 		file, err := os.OpenFile("app.log", os.O_CREATE|os.O_APPEND, 0644)
@@ -56,24 +63,34 @@ func setUpLogging() (usingFile *os.File) {
 		}
 		logTarget = file
 		usingFile = file
+		if cwd, err := os.Getwd();err == nil {
+			fmt.Printf("Logs going to %s\\%s\r\n", cwd, file.Name())
+		} else {
+			fmt.Printf("Logs going to %s\r\n", file.Name())
+		}
 		log.SetOutput(file)
 	}
 
-	infoLog = log.New(logTarget, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
-	warnLog = log.New(logTarget, "WARN ", log.Ldate|log.Ltime|log.Lshortfile)
-	critLog = log.New(logTarget, "CRITICAL ", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog = log.New(logTarget, "INFO: ", log.Ldate|log.Ltime)
+	warnLog = log.New(logTarget, "WARN: ", log.Ldate|log.Ltime)
+	critLog = log.New(logTarget, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	if onlyImportant {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+
 	return
 }
 
 func (serv *LittleServer)ServeHTTP(response http.ResponseWriter, request *http.Request){
 	// should never modify `request`
 	// `panic()` is restricted to the current request
-
-	infoLog.Printf("REQ/%s %s %s [%v]\r\n",request.Method, request.Host, request.RequestURI, request.Header)
+	infoLog.Printf("REQ/%s %s %s [%v]\r\n", request.Method, request.Host, request.RequestURI, request.Header)
 
 	// URL property has query parser built in
 	//request.URL.Query().Get("query-key") // => "query-value", given https://.../my/path?query-key=query-value
 
+	// Break path into bits, trim off empty sections at the start
 	pathBits := strings.Split(request.URL.Path, "/")
 	for len(pathBits) > 0 && pathBits[0] == "" { // trim empty sections
 		pathBits = pathBits[1:]
@@ -85,7 +102,7 @@ func (serv *LittleServer)ServeHTTP(response http.ResponseWriter, request *http.R
 	case http.MethodPost:
 		postHandler(serv, response, request, pathBits)
 	default:
-		unsupportedMethod(response)
+		unsupportedMethod(request.Method, response)
 	}
 }
 
@@ -115,7 +132,6 @@ func getHandler(serv *LittleServer, response http.ResponseWriter, pathBits []str
 		getUser(serv, pathBits[1:], response)
 
 	case "panic":
-		useLogFile=true
 		panic("panic!")
 	case "picnic":
 		picnic(response)
@@ -158,11 +174,16 @@ func getUser(serv *LittleServer, path []string, response http.ResponseWriter) {
 	if id,err:=strconv.Atoi(path[0]); err != nil{
 		invalidInput(response)
 		return
-	}else{
-		if data, err2 := json.Marshal(serv.userDb[id]); err2 != nil{
-			warnLog.Panicf("Json marshal failed: %v",err2)
+	} else {
+		if userDetails,ok := serv.userDb[id]; !ok{
+			notFound(response)
+			return
 		} else {
-			pWrite(data, response)
+			if data, err2 := json.Marshal(userDetails); err2 != nil {
+				warnLog.Panicf("Json marshal failed: %v", err2)
+			} else {
+				pWrite(data, response)
+			}
 		}
 	}
 }
@@ -182,10 +203,11 @@ func pWrite(msg []byte, response http.ResponseWriter){
 	}
 }
 
-func unsupportedMethod(response http.ResponseWriter) {
+func unsupportedMethod(method string, response http.ResponseWriter) {
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusMethodNotAllowed)
 	pWrite([]byte(`{"error":"http method not supported"}`), response)
+	warnLog.Printf("Attempt to use %s which is not supported on this path", method)
 }
 
 func homePage(response http.ResponseWriter) {
@@ -197,19 +219,21 @@ func homePage(response http.ResponseWriter) {
 func picnic(response http.ResponseWriter) {
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
-	pWrite([]byte(`{"message":"hello world"}`), response)
+	pWrite([]byte(`{"message":"hello world", "lunch":"tasty"}`), response)
 }
 
 func notFound(response http.ResponseWriter) {
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusNotFound)
 	pWrite([]byte(`{"error":"page not found"}`), response)
+	warnLog.Printf("Attempted to access an invalid path")
 }
 
 func invalidInput(response http.ResponseWriter) {
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusBadRequest)
 	pWrite([]byte(`{"error":"input is invalid"}`), response)
+	warnLog.Printf("User supplied malformed input")
 }
 
 func sendIcon(response http.ResponseWriter) {
